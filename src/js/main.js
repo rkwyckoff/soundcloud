d1e8c2cd048dbe82f22ce7de7050378c7619839c
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-19 20:30:43 -0500

    styling and js onto page

--- src/js/main.js
+++ src/js/main.js
@@ -16,15 +16,24 @@ function makeRequest(reqType) {
 
 function placeBox(data) {
   console.log(data);
-  for (var i=0; i < data.length; i++) {
+  for (var i=0; i < 20; i++) {
     var product = data[i];
+    if (!product.artwork_url) {
+      product.artwork_url = 'images/carlostocat.gif';
+    }
     var html = boxTemplate(product);
     $('.results').append(html);
   }
 }
 
 function boxTemplate(product) {
-  return `<div class="title"> ${product.title}</div>`;
+  return `
+        <div class="resultsBox">
+          <img src= "${product.artwork_url}">
+          <div class="title"> ${product.title}</div>
+          <div class="artist"> ${product.user.username}</div>
+        </div>`;
+
 }
 
 makeRequest('GET');
