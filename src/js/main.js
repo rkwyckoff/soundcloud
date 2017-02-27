ef8d8fb9b5b105e1dfb95b2452794b403971c775
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-27 16:31:00 -0500

    made the js functionality to search the sogs , then display the songs, then play individual song when clicked

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,18 @@
 import token from './token';
-
 import $ from 'jquery';
 
 // building a request  =>> reqType = 'GET', 'POST'
-function makeRequest(reqType) {
+function makeRequest(query) {
   $.ajax({
-    url: `http://api.soundcloud.com/tracks?client_id=${token}`,
+    url:  `http://api.soundcloud.com/tracks?client_id=${token}`,
     dataType: 'json',
-    method: reqType,
+    data: {
+      q: query
+    },
+    method: 'GET',
+    error: function(error) {
+      console.log('There was an error!', error);
+    },
     success: function(data) {
       placeBox(data);
     }
@@ -15,26 +20,46 @@ function makeRequest(reqType) {
 }
 
 function placeBox(data) {
-  console.log(data);
-  for (var i=0; i < 20; i++) {
+
+  for (var i=0; i < data.length; i++) {
+
     var product = data[i];
     if (!product.artwork_url) {
       product.artwork_url = 'images/carlostocat.gif';
     }
+
     var html = boxTemplate(product);
     $('.results').append(html);
   }
+  $('.resultsBox').click(getStream);
 }
 
 function boxTemplate(product) {
   return `
-
-        <div class="resultsBox">
-          <img src= "${product.artwork_url}">
-          <div class="title"> ${product.title}</div>
-          <div class="artist"> ${product.user.username}</div>
-        </div>`;
-
+    <div class="resultsBox" stream_url="${product.stream_url}">
+      <img src="${product.artwork_url}">
+      <div class="title"> ${product.title}</div>
+      <div class="artist"> ${product.user.username}</div>
+    </div>`;
 }
 
-makeRequest('GET');
+function getStream (event) {
+  $('.player').empty();
+  var url = `${event.currentTarget.attributes.stream_url.value}?client_id=${token}`;
+  console.log(url);
+  var audio  = document.createElement('audio');
+  audio.src  = url;
+  audio.id = 'audioPlayer';
+  audio.autoplay = true;
+  $('.player').append(audio);
+}
+
+function searchButton() {
+  $('.results').empty();
+  // get val of input field then log it
+  var query = $('.searchField').val();
+  makeRequest(query);
+}
+
+
+$('.searchButton').click(searchButton);
